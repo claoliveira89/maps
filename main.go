6233a28c8a0d77dd793c6b13e2947bdbdbd42cd9
Clarice Costa <claricecosta@pop-os.localdomain>
2021-04-20 18:41:23 -0300

    Declare a map variable (2)

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import "fmt"
 
 func main() {
 
-	var colors map[string]string
+	colors := make(map[string]string)
+
+	//var colors map[string]string
 	//colors := map[string]string{
 	//	"red":   "#ff0000",
 	//	"green": "#4bf745",
