de7f41ea84bb938d413e0b038cc541552dc32d7d
Clarice Costa <claricecosta@pop-os.localdomain>
2021-04-20 19:12:07 -0300

    Iterate over a map variable

--- main.go
+++ main.go
@@ -3,18 +3,17 @@ package main
 import "fmt"
 
 func main() {
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
-	colors := make(map[string]string)
+	printMap(colors)
+}
 
-	//var colors map[string]string
-	//colors := map[string]string{
-	//	"red":   "#ff0000",
-	//	"green": "#4bf745",
-	//}
-
-	colors["white"] = "#ffffff"
-
-	delete(colors, "white")
-
-	fmt.Println(colors)
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
